6cd44b78fe8c64f33ba101864daecc05f5796a10
Goncharovush <goncharovush2014@gmail.com>
2019-03-18 10:36:49 +0200

    third version

--- main.cpp
+++ main.cpp
@@ -7,17 +7,17 @@ using namespace std;
 #include <thread>
 
 int mid[1000],vwap[1000];
-int begin=2;int end=::begin+1000;
+int begin=2;int end=1002;
 int main() {
 
     auto Quote  = [](Inthread& Qt,ifstream& quote) -> void {
         int begin=::begin;
+        string **data = new string *[1000];
+        for (int i = 0; i < (1000); i++) {
+            data[i] = new string[5];
+        }
         while(!quote.eof()){
             int end=begin+1000;
-            string **data = new string *[end - begin];
-            for (int i = 0; i < (end - begin); i++) {
-                data[i] = new string[5];
-            }
             data= Qt.Reader(quote,begin,end,5);
             mid[begin-::begin]=Qt.Mid(data,(end-begin-1));
             if (begin==::end) break;
@@ -29,17 +29,16 @@ int main() {
 
     auto Trade = [](Inthread& Tr,ifstream& trade) -> void {
         int begin=::begin;
+        string **data = new string *[1000];
+        for (int i = 0; i < (1000); i++) {
+            data[i] = new string[3];
+        }
         while(!trade.eof()){
             int end=begin+1000;
-            string **data = new string *[end - begin];
-            for (int i = 0; i < (end - begin); i++) {
-                data[i] = new string[3];
-            }
             data = Tr.Reader(trade,begin,end,3);
             vwap[begin-::begin]= Tr.VWAP(data,begin,end);
             if (begin==::end) break;
             begin++;
-            delete data;
         }
 
     };
